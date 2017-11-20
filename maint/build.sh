15fad988f57c91b603db15395be308a0f1adc141
Roy Storey <kiwiroy@users.noreply.github.com>
2017-11-21 06:52:07 +1300

    build notes

--- maint/build.sh
+++ maint/build.sh
@@ -3,3 +3,14 @@
 ./scripts/fatpack-maint-build.pl \
     -source ./scripts/fatpack-maint-build.pl \
     -target ./fatpack-maint-build.pl
+
+cat - <<BUILD_INFO
+
+# **************
+perl Makefile.PL
+make manifest
+make test
+make dist
+
+# remember git clean
+BUILD_INFO
